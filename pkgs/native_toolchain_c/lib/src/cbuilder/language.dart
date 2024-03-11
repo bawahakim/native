a02364a383820fd26348098ab0f8fc2175e9f752
Moritz <mosum@google.com>
2024-03-11 14:48:33 +0100

    Add comment

--- pkgs/native_toolchain_c/lib/src/cbuilder/language.dart
+++ pkgs/native_toolchain_c/lib/src/cbuilder/language.dart
@@ -2,11 +2,14 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+import 'cbuilder.dart';
+
 /// A programming language that can be selected for compilation of source files.
+///
+/// See [CBuilder.language] for more information.
 class Language {
   /// The name of the language.
   final String name;
-
   const Language._(this.name);
 
   static const Language c = Language._('c');
