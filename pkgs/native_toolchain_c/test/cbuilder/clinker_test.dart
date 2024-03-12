036d6a15e619a7049e4650293fdaf226ad49081b
Moritz <mosum@google.com>
2024-03-12 17:13:35 +0100

    Test for size

--- pkgs/native_toolchain_c/test/cbuilder/clinker_test.dart
+++ pkgs/native_toolchain_c/test/cbuilder/clinker_test.dart
@@ -5,6 +5,8 @@
 @TestOn('linux')
 library;
 
+import 'dart:io';
+
 import 'package:native_assets_cli/native_assets_cli.dart';
 import 'package:native_toolchain_c/native_toolchain_c.dart';
 import 'package:native_toolchain_c/src/utils/run_process.dart';
@@ -21,7 +23,7 @@ void main() {
     linkInput: Uri.file('test/cbuilder/testfiles/linker/test.a'),
     linkerScript: Uri.file('test/cbuilder/testfiles/linker/symbols.lds'),
     flags: ['-u', 'my_other_func'],
-    linkerFlags: ['-strip-debug'],
+    linkerFlags: ['--strip-debug'],
   );
 
   test('link test ld', () async {
@@ -37,7 +39,10 @@ void main() {
       logger: logger,
     );
 
-    await checkResults(buildOutput);
+    // Obtained by running
+    // /usr/bin/ld -fPIC  --shared -o /tmp/libmylibname_ld_allsymbols.so --strip-debug --gc-sections --whole-archive test/cbuilder/testfiles/linker/test.a
+    const maxSize = 13760;
+    await checkResults(buildOutput, maxSize);
   });
 
   test('link test clang', () async {
@@ -53,22 +58,29 @@ void main() {
       logger: logger,
     );
 
-    await checkResults(buildOutput);
+    // Obtained by running
+    // /usr/bin/clang -fPIC  --shared -o /tmp/libmylibname_clang_allsymbols.so -Wl,--strip-debug -Wl,--gc-sections -Wl,--whole-archive test/cbuilder/testfiles/linker/test.a -Wl,--no-whole-archive
+    const sizeWithAllSymbols = 15448;
+    await checkResults(buildOutput, sizeWithAllSymbols);
   });
 }
 
-Future<void> checkResults(BuildOutput buildOutput) async {
+Future<void> checkResults(BuildOutput buildOutput, int maxSize) async {
   final filePath =
       (buildOutput.assets.first.path as AssetAbsolutePath).uri.toFilePath();
-  // final filesize = Process.runSync('du', ['-sh', filePath]).stdout;
-  final result = await runProcess(
+
+  final readelf = (await runProcess(
     executable: Uri.file('readelf'),
     arguments: ['-WCs', filePath],
     logger: logger,
-  );
-  final elf = result.stdout;
-  expect(elf, contains('my_other_func'));
-  expect(elf, isNot(contains('my_func')));
+  ))
+      .stdout;
+  expect(readelf, contains('my_other_func'));
+  expect(readelf, isNot(contains('my_func')));
+
+  final du = Process.runSync('du', ['-sb', filePath]).stdout as String;
+  final sizeInBytes = int.parse(du.split('\t')[0]);
+  expect(sizeInBytes, lessThan(maxSize));
 }
 
 BuildConfig getBuildConfig(
