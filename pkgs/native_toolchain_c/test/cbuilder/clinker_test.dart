a445295c05c7e2ad4522fb040d60d7f944d74706
Moritz <mosum@google.com>
2024-03-12 15:43:22 +0100

    Test only on linux so far

--- pkgs/native_toolchain_c/test/cbuilder/clinker_test.dart
+++ pkgs/native_toolchain_c/test/cbuilder/clinker_test.dart
@@ -2,6 +2,9 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+@TestOn('linux')
+library;
+
 import 'package:native_assets_cli/native_assets_cli.dart';
 import 'package:native_toolchain_c/native_toolchain_c.dart';
 import 'package:native_toolchain_c/src/utils/run_process.dart';
