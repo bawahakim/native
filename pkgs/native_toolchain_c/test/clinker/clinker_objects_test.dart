509214a02d93de61a5276c8a64d0633233f7d3e5
Moritz <mosum@google.com>
2024-04-11 14:53:08 +0200

    Remove object support

--- pkgs/native_toolchain_c/test/clinker/clinker_objects_test.dart
+++ pkgs/native_toolchain_c/test/clinker/clinker_objects_test.dart
@@ -23,110 +23,55 @@ Future<void> main() async {
   const os = OS.linux;
   const name = 'mylibname';
 
-  for (final precompiled in [true, false]) {
-    final ldConfig = CCompilerConfig(linker: Uri.file('/usr/bin/ld'));
-    final clangConfig = CCompilerConfig(linker: Uri.file('/usr/bin/clang'));
-    for (final cCompilerConfig in [ldConfig, clangConfig]) {
-      final linkerName = cCompilerConfig.linker!.pathSegments.last;
-      final objectSource = precompiled ? 'pre compiled' : 'freshly built';
-      test('link two objects with $linkerName and $objectSource objects',
-          () async {
-        final buildOutput = BuildOutput();
-        final tempUri = await tempDirForTest();
+  final ldConfig = CCompilerConfig(linker: Uri.file('/usr/bin/ld'));
+  final clangConfig = CCompilerConfig(linker: Uri.file('/usr/bin/clang'));
+  for (final cCompilerConfig in [ldConfig, clangConfig]) {
+    final linkerName = cCompilerConfig.linker!.pathSegments.last;
+    test('link two objects with $linkerName', () async {
+      final buildOutput = BuildOutput();
+      final tempUri = await tempDirForTest();
 
-        final objectFiles = await _objectFiles(
-          tempUri,
-          precompiled,
-          os,
-          architecture,
-        );
-        expect(objectFiles, hasLength(2));
-        logger.info(objectFiles);
+      await CBuilder.link(
+        name: name,
+        assetName: 'assetName',
+        linkerOptions: LinkerOptions.manual(
+          linkInput: [
+            Uri.file('test/clinker/testfiles/linker/test1.o'),
+            Uri.file('test/clinker/testfiles/linker/test2.o'),
+          ],
+          gcSections: false,
+        ),
+      ).run(
+        buildConfig: BuildConfig.build(
+          outputDirectory: tempUri,
+          packageName: 'testpackage',
+          packageRoot: tempUri,
+          targetArchitecture: architecture,
+          targetOS: os,
+          buildMode: BuildMode.release,
+          linkModePreference: LinkModePreference.dynamic,
+          cCompiler: cCompilerConfig,
+        ),
+        buildOutput: buildOutput,
+        logger: logger,
+      );
 
-        await CBuilder.link(
-          name: name,
-          assetName: 'assetName',
-          linkerOptions: LinkerOptions.manual(
-            linkInput: objectFiles,
-            gcSections: false,
-          ),
-        ).run(
-          buildConfig: BuildConfig.build(
-            outputDirectory: tempUri,
-            packageName: 'testpackage',
-            packageRoot: tempUri,
-            targetArchitecture: architecture,
-            targetOS: os,
-            buildMode: BuildMode.release,
-            linkModePreference: LinkModePreference.dynamic,
-            cCompiler: cCompilerConfig,
-          ),
-          buildOutput: buildOutput,
-          logger: logger,
-        );
-
-        expect(buildOutput.assets, hasLength(1));
-        final asset = buildOutput.assets.first;
-        expect(asset, isA<NativeCodeAsset>());
-        final filePath = (asset as NativeCodeAsset).file!.toFilePath();
-        expect(
-          filePath,
-          endsWith(os.dylibFileName(name)),
-        );
-        final readelf = (await runProcess(
-          executable: Uri.file('readelf'),
-          arguments: ['-WCs', filePath],
-          logger: logger,
-        ))
-            .stdout;
-        expect(readelf, contains('my_other_func'));
-        expect(readelf, contains('my_func'));
-      });
-    }
-  }
-}
-
-Future<List<Uri>> _objectFiles(
-  Uri tempUri,
-  bool preCompiled,
-  OS os,
-  Architecture architecture,
-) async {
-  if (preCompiled) {
-    return [
-      Uri.file('test/clinker/testfiles/linker/test1.o'),
-      Uri.file('test/clinker/testfiles/linker/test2.o'),
-    ];
-  } else {
-    final buildOutput = BuildOutput();
-    await CBuilder.library(
-      name: 'test1',
-      assetName: 'assetName1',
-      dartBuildFiles: [],
-      sources: [
-        packageUri
-            .resolve('test/clinker/testfiles/linker/test1.c')
-            .toFilePath(),
-        packageUri
-            .resolve('test/clinker/testfiles/linker/test2.c')
-            .toFilePath(),
-      ],
-      linkModePreference: LinkModePreference.static,
-    ).run(
-      buildConfig: BuildConfig.build(
-        outputDirectory: tempUri,
-        packageName: 'testpackage',
-        packageRoot: tempUri,
-        targetArchitecture: architecture,
-        targetOS: os,
-        buildMode: BuildMode.release,
-        linkModePreference: LinkModePreference.dynamic,
-        cCompiler: CCompilerConfig(compiler: cc),
-      ),
-      buildOutput: buildOutput,
-      logger: logger,
-    );
-
-    return buildOutput.assets.map((e) => e.file!).toList();
+      expect(buildOutput.assets, hasLength(1));
+      final asset = buildOutput.assets.first;
+      expect(asset, isA<NativeCodeAsset>());
+      final filePath = (asset as NativeCodeAsset).file!.toFilePath();
+      expect(
+        filePath,
+        endsWith(os.dylibFileName(name)),
+      );
+      final readelf = (await runProcess(
+        executable: Uri.file('readelf'),
+        arguments: ['-WCs', filePath],
+        logger: logger,
+      ))
+          .stdout;
+      expect(readelf, contains('my_other_func'));
+      expect(readelf, contains('my_func'));
+    });
   }
 }
